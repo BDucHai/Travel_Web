d0ffbd5281f4d8b96a616148b085c670c79bc67d
BDucHai <100273142+BDucHai@users.noreply.github.com>
2026-06-01 15:52:28 +0700

    Update Blog.jsx

--- FE/travel/src/Pages/Blog.jsx
+++ FE/travel/src/Pages/Blog.jsx
@@ -1,8 +1,32 @@
 import React, { useEffect, useState } from "react";
+import { CgMail } from "react-icons/cg";
+import { FaWhatsapp } from "react-icons/fa";
 
 const Blog = () => {
+    const [openContactModal, openopenContactModal] = useState(false)
     return (
-        <div>abcd</div>
+        <>
+        <div>
+            
+        </div>
+
+        {/* Floatting icon */}
+        <div className="hidden lg:fixed bottom-4 right-4 flex flex-col gap-3 z-50">
+            <div
+            onClick={() => openContactModal(true)}
+            className="bg-blue-600 text-white p-3 rounded-full shadow-lg cursor-pointer"
+            >
+                <CgMail />
+            </div>
+
+            <div className="bg-green-600 text-white p-3 rounded-full shadow-lg cursor-pointer">
+                <FaWhatsapp />
+            </div>
+      </div>
+
+      {/* Modal */}
+      <ContactModal open={openContactModal} onClose={() => setOpenContactModal(false)} />
+      </>
     )
 };
 
