af203f6271bf20c0afd2642beb83a92f254cefda
BDucHai <100273142+BDucHai@users.noreply.github.com>
2026-06-19 13:40:20 +0700

    Update uploadImage.js

--- FE/travel/src/utils/uploadImage.js
+++ FE/travel/src/utils/uploadImage.js
@@ -1,19 +1,19 @@
 import axios from "axios";
 import axiosClient from "../api/axios";
 
+// export const uploadImage = async (file) => {
+//     const formData = new FormData();
+
+//     formData.append("file", file);
+
+//     formData.append("upload_preset", "blog_upload");
+
+//     const res = await axios.post("https://api.cloudinary.com/v1_1/dtpgcvc0q/image/upload", formData);
+
+//     return { id: res?.data?.id, url: res?.data?.secure_url };
+// };
+
 export const uploadImage = async (file) => {
-    const formData = new FormData();
-
-    formData.append("file", file);
-
-    formData.append("upload_preset", "blog_upload");
-
-    const res = await axios.post("https://api.cloudinary.com/v1_1/dtpgcvc0q/image/upload", formData);
-
-    return { id: res?.data?.id, url: res?.data?.secure_url };
-};
-
-export const uploadImageBE = async (file) => {
     const formData = new FormData();
 
     formData.append("file", file);
